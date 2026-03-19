b348dda0c8af50c2d0ebc1e5e2c02fec00591181
zunaid1 <zunaidctg11@gmail.com>
2026-03-19 19:49:33 +0600

    Hero Section added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,7 +6,34 @@ import Footer from '../../components/Footer';
 const Home = () => {
 	return (
 		<div>
-			<h1> Hello from Home Page</h1>
+
+			{/* Hero */}
+			<section>
+				<div
+					className="hero min-h-screen"
+					style={{
+						backgroundImage:
+							"url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+					}}
+				>
+					<div className="hero-overlay"></div>
+					<div className="hero-content text-neutral-content text-center">
+						<div className="max-w-md">
+							<h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+							<p className="mb-5">
+								Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+								quasi. In deleniti eaque aut repudiandae et a id nisi.
+							</p>
+							<button className="btn btn-primary">Get Started</button>
+						</div>
+					</div>
+				</div>
+
+			</section>
+
+
+			{/* Time Counter */}
+			
 
 		</div>
 	);
