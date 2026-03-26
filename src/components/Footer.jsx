00bc3838a8a7292e9d523f938338d3c3e99c007a
zunaid1 <zunaidctg11@gmail.com>
2026-03-26 23:32:55 +0600

    footer fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,37 +1,40 @@
 import React from 'react';
 import LogoOnly from './LogoOnly';
+import { NavLink } from 'react-router';
+import Logo from './Logo';
 
 const Footer = () => {
+	const NavItems = <>
+		<NavLink className={({ isActive }) => (isActive ? 'text-indigo-500 border-b-3 border-indigo-500 mr-6 text-sm font-bold' : 'mr-6 text-xl font-bold')} to="/"><i className="fa-solid fa-house"></i> Home</NavLink>
+
+		<NavLink className={({ isActive }) => (isActive ? 'text-indigo-500 border-b-3 border-indigo-500 mr-6 text-sm font-bold' : 'mr-6 text-xl font-bold')} to="/allApps"> <i className="fa-brands fa-app-store"></i> Apps</NavLink>
+
+		<NavLink className={({ isActive }) => (isActive ? 'text-indigo-500 border-b-3 border-indigo-500 mr-6 text-sm font-bold' : 'mr-6 text-xl font-bold')} to="/installedApps"><i className="fa-solid fa-download"></i> Installation </NavLink>
+
+
+
+	</>
+
+
+
+
 	return (
-		<footer className="footer sm:footer-horizontal bg-base-200 text-base-content p-10">
+		<footer className="footer sm:footer-horizontal bg-slate-200 text-base-content p-10 ">
 			<aside>
-				<LogoOnly></LogoOnly>
-				<p>
-					ACME Industries Ltd.
-					<br />
-					Providing reliable tech since 1992
-				</p>
+
+				<Logo></Logo>					
 			</aside>
 			<nav>
-				<h6 className="footer-title">Services</h6>
-				<a className="link link-hover">Branding</a>
-				<a className="link link-hover">Design</a>
-				<a className="link link-hover">Marketing</a>
-				<a className="link link-hover">Advertisement</a>
+				<ul >
+					{
+						NavItems
+					}
+
+
+				</ul>
+
 			</nav>
-			<nav>
-				<h6 className="footer-title">Company</h6>
-				<a className="link link-hover">About us</a>
-				<a className="link link-hover">Contact</a>
-				<a className="link link-hover">Jobs</a>
-				<a className="link link-hover">Press kit</a>
-			</nav>
-			<nav>
-				<h6 className="footer-title">Legal</h6>
-				<a className="link link-hover">Terms of use</a>
-				<a className="link link-hover">Privacy policy</a>
-				<a className="link link-hover">Cookie policy</a>
-			</nav>
+
 		</footer>
 	);
 };
